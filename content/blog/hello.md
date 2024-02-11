9cc626fb213a587e84951713d0b4bffc4e9df3ef
Alice Vee <88582996+kamadoori@users.noreply.github.com>
2024-02-11 01:11:07 +0100

    Add @nuxt/content and create functioning example

---
+++ content/blog/hello.md
@@ -0,0 +1,8 @@
+---
+title: Hello world
+description: What's up?
+---
+
+Hello world!
+
+How are you doing?
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout>
+    <ContentDoc>
+      <template #not-found>
+        <h1>>:(</h1>
+        <span>Could not find this post.</span>
+      </template>
+      <template #default="{ doc }">
+        <article class="prose">
+          <header>
+            <h1>{{ doc.title }}</h1>
+            <p v-if="doc.description">
+              {{ doc.description }}
+            </p>
+          </header>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+    </ContentDoc>
+  </NuxtLayout>
+</template>
